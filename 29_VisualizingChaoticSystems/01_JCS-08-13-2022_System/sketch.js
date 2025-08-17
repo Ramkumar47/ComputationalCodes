fea4aa0183cc5a25f9f9a61c0ca8f1cf2f6cd6df
Ramkumar <ramkumar21194@gmail.com>
2025-08-17 17:09:36 +0530

    updated animations

--- 29_VisualizingChaoticSystems/01_JCS-08-13-2022_System/sketch.js
+++ 29_VisualizingChaoticSystems/01_JCS-08-13-2022_System/sketch.js
@@ -27,7 +27,7 @@ let cam; // camera variable
 let camResetButton;
 let simulationResetButton;
 let startSimulation=false;
-let hueCoeff = 2; // hue slope coeff. for lyapunov coloring
+// let hueCoeff = 2; // hue slope coeff. for lyapunov coloring
 
 // equation function definition
 function f(xVal,yVal,zVal){
@@ -173,11 +173,11 @@ function resetView(){
     cam.lookAt(0,0,0);
 }
 
-// hue function based on local lyapunov exponent
-function hueVal(Lval){
-    val = (1/(1+exp(-hueCoeff*Lval)))*(0-220)+220;
-    return val;
-}
+// // hue function based on local lyapunov exponent
+// function hueVal(Lval){
+//     val = (1/(1+exp(-hueCoeff*Lval)))*(0-220)+220;
+//     return val;
+// }
 
 // draw the solution curve
 function drawSolution(){
@@ -185,7 +185,8 @@ function drawSolution(){
     let xEnd,yEnd,zEnd;
     let xSize = x.length;
     for(let i=1; i<xSize; i=i+1){
-        stroke(hueVal(LL[i]),87,44);
+        // stroke(hueVal(LL[i]),87,44);
+        stroke(266,85,58);
         xStart = x[i-1]*(xMaxVal - xMinVal)+xMinVal;
         yStart = y[i-1]*(yMaxVal - yMinVal)+yMinVal;
         zStart = z[i-1]*(zMaxVal - zMinVal)+zMinVal;
